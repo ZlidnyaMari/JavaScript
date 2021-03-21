6ab4df64a140a6db876b0246a17d199b188e4744
Zlidnya <Kid08@mail.ru>
2021-03-21 22:51:18 +0300

    Задание 2

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 let Tc = +prompt('Ввeдите температуру по Цельсию');
 let Tf = (9/5)*Tc+32;
 alert ('Темература по Фарингейту =' + Tf);
+
+let admin;
+let name = 'Василий';
+admin = name;
+alert (admin);
+

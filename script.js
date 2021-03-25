a36da7a1edb667325d81bb89b4430c4cf4db1443
Zlidnya <Kid08@mail.ru>
2021-03-25 23:39:22 +0300

    Задание 4

--- script.js
+++ script.js
@@ -25,7 +25,43 @@ alert(x);
 
 let a=-5;
 let b=3;
-
 if (a>=0 && b>=0) alert(a-b);
 if (a<0 && b<0) alert(a*b);
 if (a<0 && b>=0 || a>=0 && b<0) alert(a+b); 
+
+let a=5;
+switch (a) {
+    case 1:
+        alert(1);
+    case 2:
+        alert(2);
+    case 3:
+        alert(3);
+    case 4:
+        alert(4);
+    case 5:
+        alert(5);
+    case 6: 
+        alert(6);
+    case 7:
+        alert(7); 
+    case 8: 
+        alert(8);
+    case 9:
+        alert(9); 
+    case 10:
+        alert(10); 
+    case 11:
+        alert(11); 
+    case 12:
+        alert(12); 
+    case 13:
+        alert(13); 
+    case 14:
+        alert(14); 
+    case 15:
+        alert(15);
+        break; 
+    default:
+        alert ('Нет такого значения'); // Эту запись можно как-то сократить?
+}
